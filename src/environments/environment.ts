5e15295cb7105a38bd92b85335c6500999a07d58
unknown <izacaqsha@gmail.com>
2022-10-27 18:16:40 +0700

    deployment fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: 'http://localhost:8080',
   // wsUrl: 'ws://localhost:8080/chat',
 };
